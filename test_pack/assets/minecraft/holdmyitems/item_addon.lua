7d6190ec0ad7e31413d5dcc7a0f67356acde1f33
maingvaldsen <maingvaldsen@gmail.com>
2026-02-13 18:44:10 +0100

    Made the thing

---
+++ test_pack/assets/minecraft/holdmyitems/item_addon.lua
@@ -0,0 +1,28 @@
+local deltaTime = context.deltaTime
+
+global.tweens = {};
+
+
+
+---Keyframe function for making animations without math.    
+----- // ✦ Made by maingvaldsen ✦ \\ --
+---@param tweenId number an id for your tween so you can have multiple tweens running at once and access them later
+---@param amount number the amount it should tween
+---@param time number how long it should take to tween to the new location
+---@return number tween the items new position (updated every frame)
+local function tween(tweenId, amount, time)
+    if not tweens[tweenId] then
+        tweens[tweenId] = {clock = 0}
+    end
+
+    tweens[tweenId].clock = tweens[tweenId].clock + deltaTime
+
+    local progress = M:clamp(tweens[tweenId].clock / time, 0, 1)
+    local moveValue = amount * progress
+
+    if progress >= 1 then tweens[tweenId] = nil end
+
+    return moveValue
+end
+
+debugger:out(tostring(tween(1, 10, 3)))
