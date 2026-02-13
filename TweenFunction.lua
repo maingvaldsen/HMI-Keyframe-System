29db43c674303ddc02091a27285b35cf28a07a50
maingvaldsen <maingvaldsen@gmail.com>
2026-02-13 18:45:42 +0100

    awesome

--- TweenFunction.lua
+++ TweenFunction.lua
@@ -1,4 +1,4 @@
-local deltaTime = context.deltaTime
+local deltaTime = context.deltaTime -- Remove this line if you want to use it on hmi versions before 5.1 (excluding anything below 5.0 ofc, since this stuff wasnt possible then)
 global.tweens = {};
 ---Keyframe function for making animations without math.    
 ----- // ✦ Made by maingvaldsen ✦ \\ --
